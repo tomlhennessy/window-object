6fb990d633bc3dd736c59f5bca2363d7e362a13d
Tom Hennessy <tomlhennessy@outlook.com>
2024-10-28 15:04:10 +1000

    connecting js modules

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,15 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
-
-    // Your code here
+    document.title = "Tom Hennessy's Portfolio"
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
-
-    // Your code here
+    document.querySelector("h1").innerText = "Tom Hennessy";
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-
-    // Your code here
+    document.querySelector(".section p").innerText = "I am a software developer with a passion for coding"
 }
